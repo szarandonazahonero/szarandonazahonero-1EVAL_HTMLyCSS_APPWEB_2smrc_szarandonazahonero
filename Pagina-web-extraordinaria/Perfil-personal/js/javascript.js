f1f674cac472eef1bcda135cad87b668ad5f355c
szarandonazahonero <szarandonazahonero@educa.madrid.org>
2025-04-03 02:00:12 +0200

    Confirmar

---
+++ Pagina-web-extraordinaria/Perfil-personal/js/javascript.js
@@ -0,0 +1,119 @@
+//Logica servicios
+let servicios = document.getElementById('services');
+let menuservicios = document.getElementById('menu-servicios');
+
+servicios.addEventListener('mouseover', function () {
+    menuservicios.style.display = 'block';
+});
+
+menuservicios.addEventListener('mouseover', function () {
+    menuservicios.style.display = 'block';
+});
+
+servicios.addEventListener('mouseout', function () {
+    menuservicios.style.display = 'none';
+});
+
+menuservicios.addEventListener('mouseout', function () {
+    menuservicios.style.display = 'none';
+});
+
+//Logica precios
+let prices = document.getElementById('prices');
+let menuprices = document.getElementById('menu-prices');
+
+prices.addEventListener('mouseover', function () {
+    menuprices.style.display = 'block';
+});
+
+menuprices.addEventListener('mouseover', function () {
+    menuprices.style.display = 'block';
+});
+
+prices.addEventListener('mouseout', function () {
+    menuprices.style.display = 'none';
+});
+
+menuprices.addEventListener('mouseout', function () {
+    menuprices.style.display = 'none';
+});
+
+//Logica comunity
+let comunity = document.getElementById('comunity');
+let menucomunity = document.getElementById('menu-comunity');
+
+comunity.addEventListener('mouseover', function () {
+    menucomunity.style.display = 'block';
+});
+
+menucomunity.addEventListener('mouseover', function () {
+    menucomunity.style.display = 'block';
+});
+
+comunity.addEventListener('mouseout', function () {
+    menucomunity.style.display = 'none';
+});
+
+menucomunity.addEventListener('mouseout', function () {
+    menucomunity.style.display = 'none';
+});
+
+//Logica courses
+let courses = document.getElementById('courses');
+let menucourses = document.getElementById('menu-courses');
+
+courses.addEventListener('mouseover', function () {
+    menucourses.style.display = 'block';
+});
+
+menucourses.addEventListener('mouseover', function () {
+    menucourses.style.display = 'block';
+});
+
+courses.addEventListener('mouseout', function () {
+    menucourses.style.display = 'none';
+});
+
+menucourses.addEventListener('mouseout', function () {
+    menucourses.style.display = 'none';
+});
+
+//Logica developers
+let developers = document.getElementById('developers');
+let menudevelopers = document.getElementById('menu-developers');
+
+developers.addEventListener('mouseover', function () {
+    menudevelopers.style.display = 'block';
+});
+
+menudevelopers.addEventListener('mouseover', function () {
+    menudevelopers.style.display = 'block';
+});
+
+developers.addEventListener('mouseout', function () {
+    menudevelopers.style.display = 'none';
+});
+
+menudevelopers.addEventListener('mouseout', function () {
+    menudevelopers.style.display = 'none';
+});
+
+//Logica about
+let about = document.getElementById('about');
+let menuabout = document.getElementById('menu-about');
+
+about.addEventListener('mouseover', function () {
+    menuabout.style.display = 'block';
+});
+
+menuabout.addEventListener('mouseover', function () {
+    menuabout.style.display = 'block';
+});
+
+about.addEventListener('mouseout', function () {
+    menuabout.style.display = 'none';
+});
+
+menuabout.addEventListener('mouseout', function () {
+    menuabout.style.display = 'none';
+});
